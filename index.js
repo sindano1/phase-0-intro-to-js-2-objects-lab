a95c6078f4bdaaf769021281fdf3f9221c9eef07
Sarah Indano <sarah.indano@gmail.com>
2022-02-25 13:49:55 -0500

    commit tree a1629218eff6d05c64fba04ce9d9320e9997f318

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+
+const employees = {
+    name: "firstName lastName",
+    streetAddress: "123 Boulevard Ave"
+}
+
+function updateEmployeeWithKeyAndValue(employees, key, value) {
+    //return {...employees, ...{ [key]: value}}
+    return Object.assign({}, employees, { [key]: value })
+}
